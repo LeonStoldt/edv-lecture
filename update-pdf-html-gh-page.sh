d4e365dde609f58dc7e6b71989023d0a28041393
lstoldt <leon.stoldt@bonprix.net>
2020-01-22 22:00:07 +0100

    fixed script for automatic update and moved theme file for hg-page

--- update-pdf-html-gh-page.sh
+++ update-pdf-html-gh-page.sh
@@ -1,7 +1,10 @@
+TIMESTAMP=$(date +%d.%m.%Y-%H:%M)
 WORKING_DIR=/home/leon/dev/tmp/edv-lecture
 $WORKING_DIR/print-pdf.sh
 $WORKING_DIR/generate-website.sh
 git add $WORKING_DIR/slides.pdf
+git add $WORKING_DIR/markdown/*
 git add $WORKING_DIR/static-slides/*
+git commit -m "automatic update of pdf file, website and content at $TIMESTAMP"
 git push origin master
 $WORKING_DIR/publish-gh-pages.sh
